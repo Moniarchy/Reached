e13a32f221cc0d41817bbbf2584683227707df6b
SomayaB <somaya.bounouar@gmail.com>
2018-01-18 23:56:47 -0800

    Add middleware functions to check is user is logged in

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,20 +1,24 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const authentication = require('./authentication');
+const campaigns = require('./campaigns');
+const users = require('./users');
+const { isLoggedIn, setDefaultReponseLocals } = require('../middlewares');
 
-// router.use(setDefaultReponseLocals);
+router.use(setDefaultReponseLocals);
 
 router.get('/', (request, response) => {
   if(request.session.user) {
     const id = request.session.user.id;
-    response.redirect(`/users/${id}`);
+    response.redirect(`users/${id}`);
   } else {
     response.render('index', {warning: request.flash('error')});
   }
 });
 
 
+router.use(isLoggedIn);
 router.use('/', authentication);
-// router.use('/campaigns', campaigns);
-// router.use(isLoggedIn);
+router.use('/campaigns', campaigns);
+router.use('/users', users);
 
 module.exports = router;
