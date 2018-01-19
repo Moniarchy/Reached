285a65bc0d1e4b46d1b32ab44558e4e63b4c8ec4
SomayaB <somaya.bounouar@gmail.com>
2018-01-18 17:58:01 -0800

    Add create user route with authentication

--- src/server/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', function(request, response, next) {
-  response.send('respond with a resource')
-})
-
-module.exports = router
